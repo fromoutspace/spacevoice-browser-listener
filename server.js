5ac52e91d1a829f7f8ffe31f5c4ed6ac621cfcff
a.sidko <sidkoo@outlook.com>
2019-06-03 17:58:56 +0300

    Make service configurable

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const fs = require('fs-extra');
+const path = require('path');
+const got = require('got');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const frontPagePath = path.join(__dirname, 'client', 'index.html');
+const frontPageFileContent = fs.readFileSync(frontPagePath, "utf8");
+const urlToForwardCommand = process.env.commandControllerUrl || 'http://localhost:8080';
+const port = process.env.thisServerPort || '3000';
+
+const frontPageContent = frontPageFileContent.replace('#serverPortToReplace', port);
 
 app
     .use(express.static('client'))
-    .get('/', (request, response) => response.sendFile('index.html'))
+    .use(bodyParser.text())
+    .get('/', (request, response) => response.send(frontPageContent))
+    .post('/command', (request, response) => got.post(urlToForwardCommand, {body: request.body}))
     .listen(port, () => console.log(`Voice-Listener is working on http://localhost:${port}`));
