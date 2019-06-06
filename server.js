f02eff820e5ceba149b6cd989118c863b52cb156
a.sidko <sidkoo@outlook.com>
2019-06-06 16:43:12 +0300

    Fix command sending

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const fs = require('fs-extra');
 const path = require('path');
 const got = require('got');
-const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const frontPagePath = path.join(__dirname, 'client', 'index.html');
+const frontPagePath = path.join(__dirname, 'client', 'listener.html');
 const frontPageFileContent = fs.readFileSync(frontPagePath, "utf8");
 const urlToForwardCommand = process.env.commandControllerUrl || 'http://localhost:8080';
 const port = process.env.thisServerPort || '3000';
 
 const frontPageContent = frontPageFileContent.replace('#serverPortToReplace', port);
+const doNothing = () => {};
 
 app
-    .use(express.static('client'))
-    .use(bodyParser.text())
+    .use(express.static('client'), express.json())
     .get('/', (request, response) => response.send(frontPageContent))
-    .post('/command', (request, response) => got.post(urlToForwardCommand, {body: request.body}))
+    .post('/command', (request, response) => {
+        const commands = request.body.commands;
+        if (commands) got.post(urlToForwardCommand, {body: commands[0]}).catch(doNothing)
+    })
     .listen(port, () => console.log(`Voice-Listener is working on http://localhost:${port}`));
