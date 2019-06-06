7f41dbbfeda623a2a62f36b8e0313c52bef0b284
a.sidko <sidkoo@outlook.com>
2019-06-06 18:04:10 +0300

    Add auto restart for browser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs-extra');
 const path = require('path');
 const got = require('got');
+const puppeteer = require('puppeteer');
 require('dotenv').config();
 
 const app = express();
@@ -9,9 +10,16 @@ const frontPagePath = path.join(__dirname, 'client', 'listener.html');
 const frontPageFileContent = fs.readFileSync(frontPagePath, "utf8");
 const urlToForwardCommand = process.env.commandControllerUrl || 'http://localhost:8080';
 const port = process.env.thisServerPort || '3000';
-
+const frontPageUrl = "http://localhost:" + port;
 const frontPageContent = frontPageFileContent.replace('#serverPortToReplace', port);
-const doNothing = () => {};
+const doNothing = () => {
+};
+const broserSettings = {
+    headless: false,
+    args: ['--use-fake-ui-for-media-stream']
+};
+
+let shouldReopenBrowser = true;
 
 app
     .use(express.static('client'), express.json())
@@ -20,4 +28,18 @@ app
         const commands = request.body.commands;
         if (commands) got.post(urlToForwardCommand, {body: commands[0]}).catch(doNothing)
     })
-    .listen(port, () => console.log(`Voice-Listener is working on http://localhost:${port}`));
+    .listen(port, () => {
+        console.log(`Voice-Listener is working on ${frontPageUrl}`);
+        openBrowser();
+    });
+
+
+function openBrowser() {
+    if (!shouldReopenBrowser) return;
+    (async () => {
+        const browser = await puppeteer.launch(broserSettings);
+        browser.on('disconnected', openBrowser);
+        const page = await browser.newPage();
+        await page.goto(frontPageUrl);
+    })()
+}
